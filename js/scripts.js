13c70ca70ed8f2ed15d2fe75136369649cf2749f
Ginkko <Incubusalec@gmail.com>
2015-05-29 09:30:46 -0700

    Add spec for replacing word. Passes spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 var wordReplace = function(input, target, replacement) {
   var finalOutput = ""
-
   input = input.split(" ");
+
+  for(var i = 0; i < input.length; i++) {
+    if (input[i] === target) {
+      input[i] = replacement
+    };
+  };
+
+
   finalOutput = input.join();
   return finalOutput
+
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe("wordReplace", function() {
   it("accepts an input and returns it", function() {
     expect(wordReplace("hello", "", "")).to.equal("hello");
   });
+  it("accepts an input, and replaces the target with the replacement.", function () {
+    expect(wordReplace("mellow","mellow","yellow")).to.equal("yellow")
+  });
 });
